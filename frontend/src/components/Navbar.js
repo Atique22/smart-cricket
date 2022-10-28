15e1d913c165e3ef905a244458ff98a39196595d
Atique Ahmad <bsce19008@itu.edu.pk>
2022-10-29 00:27:04 +0500

    Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
     <>
     <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-      <a href="https://flowbite.com/" className="flex items-center">
+      <a href="/" className="flex items-center">
           {/* <img src="" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo">  LOGO*/} 
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Smart Batting </span>
       </a>
