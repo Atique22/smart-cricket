d4e1461f56645de78cb1294547f31a23a139e120
Atique Ahmad <bsce19008@itu.edu.pk>
2022-11-03 01:47:51 +0500

    Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import hero_img from "../hero-img.png"
+// import 'react-bootstrap-icons';
+
 const Home = () => {
   return (
     // <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex align-items-center">
+    <section id="hero" className="d-flex align-items-center">
   
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
             <h1>Better Solutions For Your Cricket Training</h1>
             <h2>We are team of talented designers making websites with differents tools....</h2>
-            <div class="d-flex justify-content-center justify-content-lg-start">
-              <a href="/" class="btn-get-started scrollto">Get Started</a>
-              <a href="https://www.youtube.com" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+            <div className="d-flex justify-content-center justify-content-lg-start">
+              <a href="/" className="m-3"><button type="button" class="btn btn-primary btn-lg">Get Started</button></a>
+              {/* <a href="https://www.youtube.com" className="glightbox btn-watch-video"><i className="bi bi-play-circle"></i><span>Watch Video</span></a> */}
+              <a href="https://www.youtube.com" className="m-3"><button type="button" class="btn btn-outline-secondary btn-lg">Watch Video</button></a>
             </div>
           </div>
-          <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-            <img src={hero_img} class="img-fluid animated" alt="hero images"/>
+          <div className="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+            <img src={hero_img} className="img-fluid animated" alt="hero images"/>
           </div>
         </div>
       </div>
