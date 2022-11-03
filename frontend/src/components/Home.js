1148fee51e57f74c05e3f2a79fae1aa4ac0fc293
Atique Ahmad <bsce19008@itu.edu.pk>
2022-11-03 12:41:38 +0500

    Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,7 +4,7 @@ import hero_img from "../hero-img.png"
 
 const Home = () => {
   return (
-    // <!-- ======= Hero Section ======= -->
+    // ======= Hero Section ======= //
     <section id="hero" className="d-flex align-items-center">
   
       <div className="container">
@@ -13,9 +13,8 @@ const Home = () => {
             <h1>Better Solutions For Your Cricket Training</h1>
             <h2>We are team of talented designers making websites with differents tools....</h2>
             <div className="d-flex justify-content-center justify-content-lg-start">
-              <a href="/" className="m-3"><button type="button" class="btn btn-primary btn-lg">Get Started</button></a>
-              {/* <a href="https://www.youtube.com" className="glightbox btn-watch-video"><i className="bi bi-play-circle"></i><span>Watch Video</span></a> */}
-              <a href="https://www.youtube.com" className="m-3"><button type="button" class="btn btn-outline-secondary btn-lg">Watch Video</button></a>
+              <a href="/" className="m-3"><button type="button" className="btn btn-primary btn-lg">Get Started</button></a>
+              <a href="https://www.youtube.com" className="m-3"><button type="button" className="btn btn-outline-secondary btn-lg">Watch Video</button></a>
             </div>
           </div>
           <div className="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
