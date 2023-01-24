a97cd025fb04053a250ed890ac901b6d29de9e1e
Atique Ahmad <65412984+Atique22@users.noreply.github.com>
2023-01-24 20:29:22 +0500

    dashboard

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -6,15 +6,33 @@ function Chart() {
 
     return (
         <>
-            <Card style={{ width: '100', margin:"4rem" }}>
-                <Card.Img variant="top" src="../CRICKET.png" />
+            
+            <Card  className='m-3 p-4 w-full'>
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Graph </Card.Title>
+                    
+                    <canvas id="myChart" style={{width:'100%'}}>
+                    <span
+                        type="bar"
+                        data={{
+                            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                            datasets: [
+                            {
+                                label: 'GitHub Commits',
+                                backgroundColor: '#f87979',
+                                data: [40, 20, 12, 39, 10, 40, 39, 80, 40],
+                            },
+                            ],
+                        }}
+                        labels="months"
+                        />ß
+                    </canvas>
                     <Card.Text>
                         Some quick example text to build on the card title and make up the
                         bulk of the card's content.
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Button variant="primary m-2">Upload</Button>
+                    <Button variant="primary  m-2">Upload</Button>
                 </Card.Body>
             </Card>
         </>
--- frontend/src/components/DashBoardTrainingData.js
+++ frontend/src/components/DashBoardTrainingData.js
@@ -6,10 +6,15 @@ import MenuDashBoard from './DashBoardMenu'
 const TrainingDataDashboard = () => {
   return (
     //  ======= Training Data Dash-Board ======= 
-    <div id="dashboard" className=''>
+    <div id="dashboard" className='container-flex'>
+      <div className='float-left'>
+        <MenuDashBoard/>
+      </div>
+      <div className='d-flex flex-row-reverse'>
+          <Chart/>  
+      </div>
       
-      <MenuDashBoard/>
-      <Chart/>
+
     </div>
 
   )
