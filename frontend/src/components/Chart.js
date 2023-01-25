6c639309ffdf6a1f232d63d49362c16d16e59e89
Atique Ahmad <65412984+Atique22@users.noreply.github.com>
2023-01-25 18:33:12 +0500

    Graph

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import LineGraph from './LineGraph'
-function Chart() {
+function Chart(props) {
 
     return (
         <>
 
-            <Card className='m-3 p-4'>
+            <Card className='m-3 '>
                 <Card.Body>
-                    <Card.Title>Graph </Card.Title>
+                    <Card.Title>Graph {props.title}</Card.Title>
                         <LineGraph />
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                    <Card.Text>{props.description}
                     </Card.Text>
                     <Button variant="primary m-2">Upload</Button>
                     <Button variant="primary  m-2">Upload</Button>
--- frontend/src/components/DashBoardTrainingData.js
+++ frontend/src/components/DashBoardTrainingData.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Chart from './Chart'
 import MenuDashBoard from './DashBoardMenu'
 
-
 const TrainingDataDashboard = () => {
 
 
@@ -10,8 +9,13 @@ const TrainingDataDashboard = () => {
     //  ======= Training Data Dash-Board ======= 
     <div id="dashboard" className='container-flex'>
       <MenuDashBoard />
-      <div style={{ marginLeft: "7rem" }} >
-        <Chart />
+      <div className='d-flex' style={{ marginLeft: "7rem" }} >
+        <Chart title="Bubble chart" description="chart is usually used as a catchall term for the graphical representation of data. Graph refers to a chart that specifically plots data along two dimensions... " />
+        <Chart title="Bar chart" description="chart is usually used as a catchall term for the graphical representation of data. Graph refers to a chart that specifically plots data along two dimensions... " />
+      </div>
+      <div className='d-flex' style={{ marginLeft: "7rem" }} >
+        <Chart title="Line graph." description="chart is usually used as a catchall term for the graphical representation of data. Graph refers to a chart that specifically plots data along two dimensions... " />
+        <Chart title="Area graph." description="chart is usually used as a catchall term for the graphical representation of data. Graph refers to a chart that specifically plots data along two dimensions... " />
       </div>
     </div>
   )
