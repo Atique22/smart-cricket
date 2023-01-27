6ec77f28ba621f07956ff1152800c26514dbb600
Atique Ahmad <65412984+Atique22@users.noreply.github.com>
2023-01-27 20:14:46 +0500

    update

--- frontend/src/components/DashBoardTrainingData.js
+++ frontend/src/components/DashBoardTrainingData.js
@@ -2,8 +2,9 @@ import React from 'react'
 import Chart from './Chart'
 import MenuDashBoard from './DashBoardMenu'
 import FileDataDisplay from './FileDataDisplay'
-import LineGraph from './LineGraph'
-
+// import LineGraph from './LineGraph'
+import HighChartReact from './HighChartReact'
+import HighChartPie from './HighChartPie'
 const TrainingDataDashboard = () => {
 
 
@@ -12,11 +13,15 @@ const TrainingDataDashboard = () => {
     <div id="dashboard" className='container'>
        <MenuDashBoard/>
        
-      <div className='row' style={{ marginLeft: "7rem" }} >
-        <Chart title="Bubble chart" component={  <LineGraph />  } description="chart is usually used as a catchall term for the graphical representation of data. Graph refers to a chart that specifically plots data along two dimensions... " />
-        <Chart title="Bar chart"  component={  <LineGraph />  } description="chart is usually used as a catchall term for the graphical representation of data. Graph refers to a chart that specifically plots data along two dimensions... " />
-        <Chart title="Line graph."  component={  <LineGraph />  } description="chart is usually used as a catchall term for the graphical representation of data. Graph refers to a chart that specifically plots data along two dimensions... " />
-        <Chart title="Area graph."  component={  <LineGraph />  } description="chart is usually used as a catchall term for the graphical representation of data. Graph refers to a chart that specifically plots data along two dimensions... " />
+      <div className='row ' style={{ marginLeft: "7rem" }} >
+        <Chart title="Line High chart" component={  <HighChartReact />  } description="chart description if required... " />
+        <Chart title="Pie High chart" component={  <HighChartPie />  } description="chart description if required... " />
+
+        {/* <Chart title="Line High chart" component={  <HighChartReact />  } description="chart description if required... " />
+        <Chart title="Pie High chart" component={  <HighChartPie />  } description="chart description if required... " />
+        <Chart title="Bar chart"  component={  <LineGraph />  } description="chart is usually ... " />
+        <Chart title=" graph."  component={  <LineGraph />  } description="chart is usually used a... " />
+        <Chart title="Area graph."  component={  <LineGraph />  } description="chart is usually used .. " /> */}
       </div>
       <div className='border justify-content-center m-5'>
           <FileDataDisplay/>
---
+++ frontend/src/components/HighChartReact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import HighCharts from "highcharts";
+import HighChartsReact from "highcharts-react-official";
+
+const options = {
+  chart: {
+    type: "spline"
+  },
+  title: {
+    text: "My chart"
+  },
+  series: [
+    {
+      data: [1, 2, 9, 4, 3, 6, 2, 9, 4, 3, 6,]
+    }
+  ]
+};
+export default function HighChartReact (){
+
+    return(
+        <div className='col-12'>
+            <HighChartsReact highcharts={HighCharts} options={options} />
+        </div>
+    )
+
+}
